c9e6ea595046ea5b6bf7e178c740dcacfaeb802f
dotchetter <dotchetter@protonmail.ch>
2018-10-25 17:47:50 +0200

    Improved function diff_compute:
    	* iterates through .xml file and new .txt file and checks for differences
    	* appends differing values to array '_diff' and is later joined to string format as 'diffstr'
    	* error checking for undeclared variablees for each file in case they are not accessible or corrupted
    	* validates that the length of array with new entries is not the same as the old file. This tells uss that the files don't differ
    	* echoes the difference in files outside of diff_compute

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import io
+
 import os
 import xml.etree.ElementTree as et
 
 def diff_compute():
 
+	diffstr = None
+	_new_entries = []
+	_old_entries = []
 	_diff = []
-	_origin = []
-	_pdo_export = []
-	_iter = None
 
 	# establish current path
 	_base_path = os.path.dirname(os.path.realpath(__file__))
-	# grabbing .xml file
-	_xml_file = os.path.join(_base_path, "GADS_adm.xml")
 	# grabbing the manifest with all 'physicaldeliveryofficename' entries 
-	_pdo_file_path = os.path.join(_base_path, "PDONoutput.txt")
-	_pdo_file = io.open(_pdo_file_path, mode='r', encoding='utf-8')
+	_pdo_file = open(os.path.join(_base_path,'PDONoutput.txt'),'r',encoding = 'utf-8')
+	# grabbing .xml file
+	_xml_file = os.path.join(_base_path,'GADS_adm.xml')
 	# parsing the file to get contents
 	_tree = et.parse(_xml_file)
 	# defining what we're looking for here. The PDO's are listed in 'orgName' children
-	_root = _tree.findall(".//orgName")
+	_subtrees = _tree.findall(".//orgName")
+	
+	if not _pdo_file or not _subtrees:
+		raise AttributeError(
+			'An error occured when iterating through .xml file or attribute manifest file'
+			)	
+	for i in _pdo_file: _new_entries.append(i)
+	for i in _subtrees: _old_entries.append(i.text)
 
-	# build arrays of data for comparison
-	for i in _root:
-		_iter = i.text
-		_origin.append(_iter)
-	for n in _pdo_file:
-		n = str(n)
-		_pdo_export.append(n)
+	if len(_new_entries) != len(_old_entries) and len(_new_entries) != 0:
+		_new_entries.sort()
+		_old_entries.sort()
+		for pdo in _new_entries:
+			if not pdo in _old_entries:	_diff.append(pdo)
 
-	for i in _origin:
-		if i not in _pdo_export:
-			print(i,'is not in the new file')
+		diffstr = "".join(_diff)
+		return diffstr
+	else:
+		return 'No data to add to new file.'
 
 if __name__ == '__main__':
-	diff_compute()
+	the_diff = diff_compute()
+	if the_diff != 'No data to add to new file.':
+		print('\n','>> There are new entries. These are:','\n','\n' + the_diff)
+	else:
+		print(the_diff)
