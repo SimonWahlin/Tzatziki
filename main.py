1b9060fad3162c33e7f32909ed998093450ff12a
dotchetter <dotchetter@protonmail.ch>
2018-10-31 17:24:15 +0100

    Rebuilt diff_compute() and created build_from_xml() to handle a filtering of the xml file, fetching the data of interest to a separate file being read from.

--- main.py
+++ main.py
@@ -2,52 +2,47 @@
 import os
 import xml.etree.ElementTree as ET
 
+
+def build_from_xml():
+
+	_base_path = os.path.dirname(os.path.realpath(__file__))
+		
+	_xml_tree = ET.parse(os.path.join(_base_path,'GADS_adm.xml'))
+	
+	_subtrees = _xml_tree.getroot()
+	
+	_new_file_raw = open(os.path.join(_base_path,'new_file.dat'),'r',encoding = 'utf-8')
+	_new_file = _new_file.readlines()
+
+	_old_file_raw = open(os.path.join(_base_path,'old_attrib.txt',)'w',encoding = 'utf-8')
+	
+	# building the text file
+	for pdo in _subtrees.iter('orgName'): _old_file_raw.write(pdo.text + '\n')
+	_old_file = _old_file_raw.readlines()
+
+
 def diff_compute():
 
-	diffstr = None
 	_new_entries = []
-	_old_entries = []
 	_diff = []
 
-	# establish current path
-	_base_path = os.path.dirname(os.path.realpath(__file__))
-	
-	# grabbing the manifest with all 'physicaldeliveryofficename' entries 
-	_pdo_file = open(os.path.join(_base_path,'PDONoutput.txt'),'r',encoding = 'utf-8')
-		
-	# parsing the file to get contents
-	_tree = ET.parse(os.path.join(_base_path,'GADS_adm.xml'))
-	_outfile = file.write(os.path.join(_base_path,''))
-	# defining what we're looking for here. The PDO's are listed in 'orgName' children
-	_subtrees = _tree.getroot()
-	
-	for orgname in _subtrees.iter('orgName'):
-		i = str(orgname.text)
-		
+	if not _new_file or not _subtrees:
+		raise AttributeError(
+			'An error occured when iterating through .xml file or attribute manifest file'
+			)	
+	for i in _new_file_lines: _new_entries.append(i)
+	for i in _subtrees: _old_entries.append(i)
 
-	for orgname in _pdo_file.iter():
-		if orgname in _old_entries:
-			print(orgname)
-		else:
-			print('no match')
+	if len(_new_entries) != len(_old_entries) and len(_new_entries) != 0:
+		_new_entries.sort()
+		_old_entries.sort()
+		for pdo in _new_entries:
+			if pdo in _old_entries:	_diff.append(pdo)
 
-	# if not _pdo_file or not _subtrees:
-	# 	raise AttributeError(
-	# 		'An error occured when iterating through .xml file or attribute manifest file'
-	# 		)	
-	# for i in _pdo_file_lines: _new_entries.append(i)
-	# for i in _subtrees: _old_entries.append(i)
-
-	# if len(_new_entries) != len(_old_entries) and len(_new_entries) != 0:
-	# 	_new_entries.sort()
-	# 	_old_entries.sort()
-	# 	for pdo in _new_entries:
-	# 		if pdo in _old_entries:	_diff.append(pdo)
-
-	# 	diffstr = "".join(_diff)
-	# 	return diffstr
-	# else:
-	# 	return 'No data to add to new file.'
+		diffstr = "".join(_diff)
+		return diffstr
+	else:
+		return 'No data to add to new file.'
 
 if __name__ == '__main__':
 	diff_compute()
@@ -57,3 +52,4 @@ if __name__ == '__main__':
 	# 	print('\n','>> There are new entries. These are:','\n','\n' + the_diff)
 	# else:
 	# 	print(the_diff)
+
