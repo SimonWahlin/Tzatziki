10da68433ecca405b48fe480cfe1433f31a99ab2
dotchetter <dotchetter@protonmail.ch>
2018-11-01 16:51:28 +0100

    Now working comparison function 'diff_compute()'.

--- main.py
+++ main.py
@@ -3,53 +3,58 @@ import os
 import xml.etree.ElementTree as ET
 
 
-def build_from_xml():
-
+def diff_compute(xml,newfile):
+	diff = []
 	_base_path = os.path.dirname(os.path.realpath(__file__))
-		
-	_xml_tree = ET.parse(os.path.join(_base_path,'GADS_adm.xml'))
-	
+	_xml_tree = ET.parse(os.path.join(_base_path,xml))
 	_subtrees = _xml_tree.getroot()
-	
-	_new_file_raw = open(os.path.join(_base_path,'new_file.dat'),'r',encoding = 'utf-8')
-	_new_file = _new_file.readlines()
 
-	_old_file_raw = open(os.path.join(_base_path,'old_attrib.txt',)'w',encoding = 'utf-8')
-	
-	# building the text file
-	for pdo in _subtrees.iter('orgName'): _old_file_raw.write(pdo.text + '\n')
-	_old_file = _old_file_raw.readlines()
+	try:
+		# building dat file with old attributes from the .xml file
+		_old_file_raw = open(os.path.join(_base_path,'old_attrib.dat'),'w',encoding = 'utf-8')
+		for pdo in _subtrees.iter('orgName'): _old_file_raw.write(pdo.text + '\n')
+		_old_file_raw.close()
+		
+		_old_file_raw = open(os.path.join(_base_path,'old_attrib.dat'),'r',encoding = 'utf-8')
+		_old_file = _old_file_raw.readlines()
+		_old_file_raw.close()
+	except:
+		raise RuntimeError ('An error occured with generating the .dat file from .xml data. Verify permissions and access.')
+	try:
+		# opening dat file with new attributes
+		_new_file_raw = open(os.path.join(_base_path,newfile),'r',encoding = 'utf-8')
+		_new_file = _new_file_raw.readlines()
+		_new_file_raw.close()
+	except:
+		if not _new_file or not _old_file: 
+			raise RuntimeError ('An error occured. Is the .xml file and new attrib file there?')
+
+	for pdo in _new_file:
+		if not pdo in _old_file:
+			diff.append(pdo)
+
+	if diff and len(diff) > 1: 
+		return diff
+	else: 
+		return 'None'
 
 
-def diff_compute():
 
-	_new_entries = []
-	_diff = []
+def main():
 
-	if not _new_file or not _subtrees:
-		raise AttributeError(
-			'An error occured when iterating through .xml file or attribute manifest file'
-			)	
-	for i in _new_file_lines: _new_entries.append(i)
-	for i in _subtrees: _old_entries.append(i)
+	diff_len = 0
+	diff = diff_compute('GADS_ADM.xml','new_attrib.dat')
 
-	if len(_new_entries) != len(_old_entries) and len(_new_entries) != 0:
-		_new_entries.sort()
-		_old_entries.sort()
-		for pdo in _new_entries:
-			if pdo in _old_entries:	_diff.append(pdo)
-
-		diffstr = "".join(_diff)
-		return diffstr
+	if not diff == 'None':
+		diff_len = len(diff)
+		print('There are ',str(diff_len),' new attributes:','\n')
+		print(diff)
 	else:
-		return 'No data to add to new file.'
+		print('No diff today - .xml file manifest is up to date.')
+
+	# Build .xml data blocks for each new entry. must resemble data block found in orgName children in GADS file.
+	# Requires numeric counter for every block of data, later used as '<priority>' in xml.
+
 
 if __name__ == '__main__':
-	diff_compute()
-
-	# the_diff = diff_compute()
-	# if the_diff != 'No data to add to new file.':
-	# 	print('\n','>> There are new entries. These are:','\n','\n' + the_diff)
-	# else:
-	# 	print(the_diff)
-
+	main()
