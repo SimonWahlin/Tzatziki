f57b85a57068d555535d5099def07c8bc17e1d0b
dotchetter <dotchetter@protonmail.ch>
2018-11-05 21:57:01 +0100

    diff_compute() is now capable of accurately calculating the difference between the data in source xml file and the new file being compared with. The data is returned in array diff[]. For every differing line in the data (new lines added), an .xml structure is needed and then appended to the .xml file. This is done with class Build.

---
+++ Build.py
@@ -0,0 +1,12 @@
+class Build(object):
+	
+	'''
+	Builds .xml data sets for each entry of data being given. 
+	Intended use is to build generic repetitive .xml data blocks for each line
+	in a calculated difference between two files.
+	'''
+	pass
+	def __init__(self, diff):
+		# self.block_arr = []
+		pass
+		# 
--- main.py
+++ main.py
@@ -3,58 +3,56 @@ import os
 import xml.etree.ElementTree as ET
 
 
-def diff_compute(xml,newfile):
+def diff_compute(xmlsrc, newsrc):
+	'''
+	diff_compute takes xml file being parsed and the file containing new data as arguments.
+	The data of interest in the xml file is located in elements where 'OrgName' is described. 
+	The data is written to a file with encoding to preserve non-ascii characters. The data is 
+	then imported as a set, to sort by unique and to allow for mathematical actions on the data.
+	The temp file exported is then
+	'''
 	diff = []
-	_base_path = os.path.dirname(os.path.realpath(__file__))
-	_xml_tree = ET.parse(os.path.join(_base_path,xml))
-	_subtrees = _xml_tree.getroot()
+	base_path = os.path.dirname(os.path.realpath(__file__))
+	xml_tree = ET.parse(os.path.join(base_path, xmlsrc))
+	subtrees = xml_tree.getroot()
 
 	try:
-		# building dat file with old attributes from the .xml file
-		_old_file_raw = open(os.path.join(_base_path,'old_attrib.dat'),'w',encoding = 'utf-8')
-		for pdo in _subtrees.iter('orgName'): _old_file_raw.write(pdo.text + '\n')
-		_old_file_raw.close()
-		
-		_old_file_raw = open(os.path.join(_base_path,'old_attrib.dat'),'r',encoding = 'utf-8')
-		_old_file = _old_file_raw.readlines()
-		_old_file_raw.close()
+		old_file_raw = open(os.path.join(base_path, 'old_attrib.dat'), 'w', encoding = 'utf-8')
+		for data in subtrees.iter('orgName'):
+			old_file_raw.write(data.text + '\n')
+		old_file_raw.close()
+		old_file = open('old_attrib.dat', 'r', encoding = 'utf-8').read().split('\n')
+		old_set = set(old_file)
+		new_file = open(os.path.join(base_path, newsrc), 'r', encoding = 'utf-8').read().split('\n')
+		new_set = set(new_file)
+		for data in new_set:
+			if not data in old_set and not '\ufeff' in data:
+				diff.append(data)
+		if diff and len(diff) > 1: 
+			return diff
+		else: 
+			return 'None'
 	except:
-		raise RuntimeError ('An error occured with generating the .dat file from .xml data. Verify permissions and access.')
-	try:
-		# opening dat file with new attributes
-		_new_file_raw = open(os.path.join(_base_path,newfile),'r',encoding = 'utf-8')
-		_new_file = _new_file_raw.readlines()
-		_new_file_raw.close()
-	except:
-		if not _new_file or not _old_file: 
-			raise RuntimeError ('An error occured. Is the .xml file and new attrib file there?')
-
-	for pdo in _new_file:
-		if not pdo in _old_file:
-			diff.append(pdo)
-
-	if diff and len(diff) > 1: 
-		return diff
-	else: 
-		return 'None'
-
-
+		raise RuntimeError ('Fatal error: something went wrong while building data sets of files')
 
 def main():
-
+	# define names for the .xml file to parse and the dat file with new attributes to compare with
+	xml_file = 'GADS_ADM.xml'
+	new_file = 'new.dat'
 	diff_len = 0
-	diff = diff_compute('GADS_ADM.xml','new_attrib.dat')
+	datablocks = []
+
+	diff = diff_compute(xml_file, new_file)
 
 	if not diff == 'None':
 		diff_len = len(diff)
-		print('There are ',str(diff_len),' new attributes:','\n')
-		print(diff)
+		print('\n', '* There are ',str(diff_len),' new attributes:','\n')
+		for line in diff: 
+			print(line, sep = ' ', end = '\n')
+		# datablocks = Build(diff) >> Not built! <<
+
 	else:
 		print('No diff today - .xml file manifest is up to date.')
-
-	# Build .xml data blocks for each new entry. must resemble data block found in orgName children in GADS file.
-	# Requires numeric counter for every block of data, later used as '<priority>' in xml.
-
 
 if __name__ == '__main__':
 	main()
