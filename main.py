dbb73246a0faea0496b5140c82dd0b3c6959d080
dotchetter <dotchetter@protonmail.ch>
2018-12-09 23:20:50 +0100

    Ã * Added prettifying indentation and new lines when creating new subelements *  Bug fix for highest_priority_int where exception would raise if no elements of interest were found in the file. In this case, it now starts from 0 instead of crashing. * docstring update for each function

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import os
 import xml.etree.ElementTree as ET
 
 def main():
-
-	# file containing new data: name
-	new = 'new.dat'
-	# xml file: name
+	''' Get the data necessary from other functions. 
+		Tree and root objects are created from the xml
+		file being parsed. A set for data from the xml
+		file (old_set) and a set for data from the input 
+		file (new_set) are created and every attribute in 
+		the xml file containing the data sought after in
+		orgName for every "search" element, is compared
+		to find missing data in the xml file compared to
+		data in the input file. This allows for a freshly
+		updated xml file with the latest data. The input
+		file is exported from LDAP server, which is being
+		compared with the contents in the xml file here. '''
+	new = 'new.dat'	
 	xml_name = 'GADS_ADM.xml'
-	# get current filepath of execution
 	base_path = os.path.dirname(os.path.realpath(__file__))
-	# parse xml file for tree
 	tree = get_xml_tree(base_path, xml_name)
-	# parse xml file for every element in the file
 	root = get_xml_root(tree)
-	# fetch text data of interest from xml file to set
 	old_set = get_old_data(root)
-	# fetch text data of interest from text file to set
 	new_set = get_new_data(base_path, new)
-	# get the highest value 'priority' attribute in xml file
 	priority_int = highest_priority_int(root)
-	# get set containing only the data present in text file but not xml file
 	diff = compute_diff(old_set, new_set)
-
 	if diff != 'None':
 		build_element(base_path, xml_name, root, diff, priority_int, tree)
 
@@ -41,24 +42,28 @@ def get_xml_tree(base_path, xml_name):
 
 
 def get_old_data(root):
-	''' Get every line of data from the 'orgName'  attribute in root/plugins/local/plugin/config/users
-		from the .xml file. This allows for mathematical comparison with the data from the LDAP server.
-	'''
+	''' Get every line of data from the 'orgName' attribute
+		in root/plugins/local/plugin/config/users
+		from the .xml file. This allows for mathematical 
+		comparison with the data from the LDAP server. '''
 	old = set()
 	try:
 		for line in root.iter('orgName'):
 			old.add(line.text)
 		return old
 	except:
-		raise RuntimeError ('Error occured while parsing building set from xml data')
+		raise RuntimeError (
+			'Error occured while parsing building set from xml data'
+		)
 
 
 def get_new_data(base_path, new_source):
-	''' Get every line of data from the file containing data exported from the LDAP server. 
-		This data will contain attributes from AD / Catalog service which needs to be included
-		in the .xml file. Adding them to a set() allows for mathematical comparison.
-	'''
-
+	''' Get every line of data from the file containing 
+		data exported from the LDAP server. 
+		This data will contain attributes from AD / Catalog 
+		service which needs to be included
+		in the .xml file. Adding them to a set() allows for 
+		mathematical comparison. '''
 	new = set()
 	_new_filepath = os.path.join(base_path, new_source)
 	try:
@@ -69,76 +74,100 @@ def get_new_data(base_path, new_source):
 					new.add(line)
 			return new
 	except:
-		raise RuntimeError ('Error occured while parsing file with new data')
+		raise RuntimeError (
+			'Error occured while parsing file with new data'
+		)
 
 
 def highest_priority_int(root):
-	''' Find the last and highest integer in attribute 'Priority' for every element of interest. 
-		this value is later used to increase from when creating new subelements in the .xml file.
-	'''
-	
+	''' Find the last and highest integer in attribute 
+		'Priority' for every element of interest. 
+		this value is later used to increase from when 
+		creating new subelements in the .xml file. 
+		If no previous entries are found, 0 is returned to
+		start building elements from. '''
 	_prio_arr = []
 	highest = int()
-	# get root element for file ('filename')
-	prio_int = root.findall('plugins/local/plugin/config/users/search/priority')
-	for elem in prio_int:
-		num = int(elem.text)
-		_prio_arr.append(num)
-	highest = max(_prio_arr)
-	return highest
-
+	prio_int = root.findall(
+		'plugins/local/plugin/config/users/search/priority'
+	)
+	if prio_int and prio_int > 0:
+		for elem in prio_int:
+			num = int(elem.text)
+			_prio_arr.append(num)
+		highest = max(_prio_arr)
+		return highest
+	else:
+		return int(0)
 
 def compute_diff(old, new):
-	''' Calculate the difference between the sets being passed to this function.
-		The sets of data contains every line of text with an attribute name fetched from LDAP server
-		which is being synced to the G Suite domain. Each line must be in the .xml file for to be included 
-		in the sync. We check if the name appears in the .xml file already by comparing these two sets of data.
-	'''
+	''' Calculate the difference between the sets being passed 
+		to this function. The sets of data contains every line 
+		of text with an attribute name fetched from LDAP server 
+		which is being synced to the G Suite domain. Each line 
+		must be in the .xml file for to be included in the sync.
+		We check if the name appears in the .xml file already by 
+		comparing these two sets of data. '''
 	diff = []
 	try:
 		for line in new:
-			if not line in old and not '\ufeff' in line: # ignore encoding header in text file
+			if not line in old and not '\ufeff' in line:
 				diff.append(line)
 		if len(diff):
 			return diff
 		else: 
 			return 'None'
 	except:
-		raise RuntimeError ('Something went wrong while building data sets for comparison')
+		raise RuntimeError (
+			'Something went wrong while building data sets for comparison'
+		)
 
 
 def build_element(base_path, xml_name, root, diff, prio_int, tree):
-	''' Build new subelements for data in diff[] array passed to this function.
-		The source file is used and appended to. Every Attribute 'priority' is sequencially increased
-		to give elements correct priority integer. Every Element is given an identical text string in 'filter'
-		attribute but with a unique ending; it's own 'orgname' text string. This is used by GADS to create
-		organizational units in G Suite.
-	'''  
-
-	_filter_string = "(&amp;(objectCategory=person)(objectClass=user)(mail=*katrineholm.se*)(physicalDeliveryOfficeName="
+	''' Build new xml subelements for data in diff[] array passed 
+		to this function. The source file is used and appended to. 
+		Every 'priority' attribute is sequencially increased to 
+		give elements correct priority integer with prio_int. 
+		Every Element is given an identical text string in 'filter'
+		attribute but with a unique ending; it's own 'orgname' text 
+		string. This is used by GADS to create OU's in G Suite.	'''  
 	
+	_filter_string = "(&amp;(objectCategory=person)(objectClass=user)" \
+					"(mail=*katrineholm.se*)(physicalDeliveryOfficeName="
+	_attrib_tail = ('\n' + ('\t'*6 + ' '))
+	_emlem_tail = ('\n'*2 + ('\t'*6))
+	_last_attrib_tail = ('\n' + ('\t'*6))
+	source_file = (os.path.join(base_path, xml_name))
+	parent = root.find('plugins/local/plugin/config/users')
 	try:
-		source_file = (os.path.join(base_path, xml_name))
-		parent = root.find('plugins/local/plugin/config/users')
-	
 		for data in diff:
 			prio_int += 1
-			# create attributes in new 'search' element:
 			elem = ET.SubElement(parent, "search")
 			elem_priority = ET.SubElement(elem, "priority")
 			elem_suspended = ET.SubElement(elem, "suspended")
 			elem_scope = ET.SubElement(elem, "scope")
 			elem_orgname = ET.SubElement(elem, "orgName")
 			elem_filter = ET.SubElement(elem, "filter")
-			# contents in each attribute
+	
 			elem_priority.text = str(prio_int)
 			elem_suspended.text = "false"
 			elem_scope.text = "SUBTREE"
 			elem_orgname.text = data
 			elem_filter.text = (_filter_string + data + "))")
+
+			elem_priority.tail = _attrib_tail
+			elem_suspended.tail = _attrib_tail
+			elem_scope.tail = _attrib_tail
+			elem_orgname.tail = _attrib_tail
+			elem_filter.tail = _attrib_tail
+			elem.tail = _last_attrib_tail
+
 		tree.write(source_file, encoding='utf-8')
+		print(len(diff), 'new elements were created.')
 	except:
-		raise RuntimeError ('Could not create new elements in file', xml_name)
+		raise RuntimeError (
+			'Could not create new elements in file', xml_name
+		)
 
 if __name__ == '__main__':
 	main()
