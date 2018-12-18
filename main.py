0b84c08215b15713df91d5c734e3c39453db19f9
dotchetter <dotchetter@protonmail.ch>
2018-12-19 00:10:29 +0100

    CÃleaned up cluttring docstrings. Refactored functions and code style for cleaner view.

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 
-
 import os
 import xml.etree.ElementTree as ET
 
 def main():
-	''' Get the data necessary from other functions. 
-		Tree and root objects are created from the xml
-		file being parsed. A set for data from the xml
-		file (old_set) and a set for data from the input 
-		file (new_set) are created and every attribute in 
-		the xml file containing the data sought after in
-		orgName for every "search" element, is compared
-		to find missing data in the xml file compared to
-		data in the input file. This allows for a freshly
-		updated xml file with the latest data. The input
-		file is exported from LDAP server, which is being
-		compared with the contents in the xml file here. '''
 	new = 'new.dat'	
 	xml_name = 'GADS_ADM.xml'
 	base_path = os.path.dirname(os.path.realpath(__file__))
@@ -33,25 +20,27 @@ def main():
 
 def get_xml_tree(base_path, xml_name):
 	''' Parse the root from .xml file, return tree hierarchy.'''
+	
 	try:
 		tree = ET.parse(os.path.join(base_path, xml_name))
 		return tree
 	except:
 		raise FileNotFoundError (
-			'Could not find xml file in current directory.'
-		)
+			'Could not find xml file in current directory.')
+
 
 def get_xml_root(tree):
 	''' Parse .xml file, return the root element for the file. '''
+	
 	_root = tree.getroot()
 	return _root
 
 
 def get_old_data(root):
-	''' Get every line of data from the 'orgName' attribute
-		in root/plugins/local/plugin/config/users
-		from the .xml file. This allows for mathematical 
-		comparison with the data from the LDAP server. '''
+	''' Get every line of data from the 'orgName' attribute	in 
+		root/plugins/local/plugin/config/users from the .xml file. This allows for 
+		mathematical comparison with the data from the LDAP server. '''
+	
 	old = set()
 	try:
 		for line in root.iter('orgName'):
@@ -59,17 +48,15 @@ def get_old_data(root):
 		return old
 	except:
 		raise RuntimeError (
-			'Error occured while parsing building set from xml data'
-		)
+			'Error occured while parsing building set from xml data')
 
 
 def get_new_data(base_path, new_source):
-	''' Get every line of data from the file containing 
-		data exported from the LDAP server. 
-		This data will contain attributes from AD / Catalog 
-		service which needs to be included
-		in the .xml file. Adding them to a set() allows for 
+	''' Get every line of data from the file containing data exported from the LDAP 
+		server. This data will contain attributes from AD / Catalog service which 
+		needs to be included in the .xml file. Adding them to a set() allows for 
 		mathematical comparison. '''
+	
 	new = set()
 	_new_filepath = os.path.join(base_path, new_source)
 	try:
@@ -81,39 +68,32 @@ def get_new_data(base_path, new_source):
 			return new
 	except:
 		raise RuntimeError (
-			'Error occured while parsing file with new data'
-		)
+			'Error occured while parsing file with new data')
 
 
 def highest_priority_int(root):
-	''' Find the last and highest integer in attribute 
-		'Priority' for every element of interest. 
-		this value is later used to increase from when 
-		creating new subelements in the .xml file. 
-		If no previous entries are found, 0 is returned to
-		start building elements from. '''
+	''' Find the last and highest integer in attribute 'Priority' for every element
+		of interest. This value is later used to increase from when creating new 
+		subelements in the .xml file. If no previous entries are found, 0 is returned 
+		to start building elements from. If no element is present, start from 0. '''
+
 	_prio_arr = []
-	highest = int()
-	prio_int = root.findall(
-		'plugins/local/plugin/config/users/search/priority'
-	)
-	if prio_int and len(prio_int) > 0:
-		for elem in prio_int:
-			num = int(elem.text)
-			_prio_arr.append(num)
+	_prio_int = root.findall(
+		'plugins/local/plugin/config/users/search/priority')
+	if _prio_int and len(_prio_int) > 0:
+		for elem in _prio_int:
+			_prio_arr.append(int(elem.text))
 		highest = max(_prio_arr)
 		return highest
 	else:
 		return int(0)
 
+
 def compute_diff(old, new):
-	''' Calculate the difference between the sets being passed 
-		to this function. The sets of data contains every line 
-		of text with an attribute name fetched from LDAP server 
-		which is being synced to the G Suite domain. Each line 
-		must be in the .xml file for to be included in the sync.
-		We check if the name appears in the .xml file already by 
-		comparing these two sets of data. '''
+	''' Calculate the difference between the sets being passed to this function. 
+		The sets of data contains every line of text with an attribute name fetched 
+		from LDAP server which is being synced to the G Suite domain. '''
+	
 	diff = []
 	try:
 		for line in new:
@@ -124,18 +104,16 @@ def compute_diff(old, new):
 		return diff if len(diff) else 'None'
 	except:
 		raise RuntimeError (
-			'Something went wrong while building data sets for comparison'
-		)
+			'Something went wrong while building data sets for comparison')
 
 
 def build_element(base_path, xml_name, root, diff, prio_int, tree):
-	''' Build new xml subelements for data in diff[] array passed 
-		to this function. The source file is used and appended to. 
-		Every 'priority' attribute is sequencially increased to 
-		give elements correct priority integer with prio_int. 
-		Every Element is given an identical text string in 'filter'
-		attribute but with a unique ending; it's own 'orgname' text 
-		string. This is used by GADS to create OU's in G Suite.	'''  
+	''' Build new xml subelements for data in diff[] array passed to this function. 
+		The source file is used and appended to. Every 'priority' attribute is 
+		sequencially increased to give elements correct priority integer with prio_int. 
+		Every Element is given an identical text string in 'filter'	attribute but with
+		a unique ending; it's own 'orgname' text string. This is used by GADS to create 
+		OU's in G Suite.'''  
 	
 	_filter_string = "(&amp;(objectCategory=person)(objectClass=user)" \
 					"(mail=*katrineholm.se*)(physicalDeliveryOfficeName="
